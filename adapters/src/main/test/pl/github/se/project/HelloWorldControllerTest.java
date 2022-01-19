7ac84ba34d03235cf51ec37877caa756d94b0063
MOstrega <michalostrega@gmail.com>
2022-01-19 13:14:27 +0100

    pipe test

---
+++ adapters/src/main/test/pl/github/se/project/HelloWorldControllerTest.java
@@ -0,0 +1,13 @@
+package pl.github.se.project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldControllerTest {
+
+    @Test
+    void getHelloWorld() {
+        assertEquals(0,1);
+    }
+}
