30dee85faf224c3f9ce0252bfdd45f290aab5ab6
MOstrega <michalostrega@gmail.com>
2022-01-19 12:01:21 +0100

    Hello World

---
+++ adapters/src/main/java/pl/github/se/project/HelloWorldController.java
@@ -0,0 +1,22 @@
+package pl.github.se.project;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.github.se.project.dto.ProjectDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+class HelloWorldController {
+
+    @GetMapping
+    String getHelloWorld() {
+        return "Hello world";
+    }
+
+}
