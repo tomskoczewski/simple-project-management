27a0a7a53cd0953fabf42933c1bd38ab95f901e6
MOstrega <michalostrega@gmail.com>
2022-01-24 23:08:00 +0100

    Database initialization by data.sql file

---
+++ adapters/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO SE_PROJECTS (name, description, days_to_deadline) VALUES
+                                                      ('USE CASES', 'Use Cases for software Enginering ', 3),
+                                                      ('USE CASES UML', 'Use Cases UML for software Enginering ', 3),
+                                                      ('CLASS DIAGRAM UML', 'CLASS DIAGRAM UML for software Enginering', 5);
